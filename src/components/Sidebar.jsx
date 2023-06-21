156dff21f273ddadabd697d380dce5ce32d26bcf
Neander de Souza <neander.faria@hotmail.com>
2023-06-21 14:36:34 -0300

    Componente: Avatar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,7 @@
 import { PencilLine } from 'phosphor-react';
 
+import { Avatar } from './Avatar';
+
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
@@ -11,7 +13,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/neanderdev.png" />
+        <Avatar src="https://github.com/neanderdev.png" />
 
         <strong>Neander de Souza</strong>
 
