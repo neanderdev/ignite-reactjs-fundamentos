2bff5a4ae7a1b0d5f4fd8bdb601b703c1c83388f
Neander de Souza <neander.faria@hotmail.com>
2023-06-21 14:33:29 -0300

    Estilizações do Comentário

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -17,7 +17,7 @@ export function Comment() {
             </div>
 
             <button title="Deletar comentário">
-              <Trash size={20} />
+                <Trash size={24} />
             </button>
           </header>
 
@@ -27,7 +27,7 @@ export function Comment() {
         <footer>
           <button>
             <ThumbsUp />
-            
+
             Aplaudir <span>20</span>
           </button>
         </footer>
