d119d793ea31a5b280a62e9d03cde413c4d70597
Neander de Souza <neander.faria@hotmail.com>
2023-06-21 14:19:53 -0300

    Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 import { Post } from './Post';
+
+import styles from './App.module.css';
 
 import './global.css';
 
@@ -7,16 +10,22 @@ export function App() {
   return (
     <div>
       <Header />
-      
-      <Post
-        author="Neander de Souza"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
-      />
 
-      <Post
-        author="John Doe"
-        content="Um novo post muito legal"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        
+        <main>
+          <Post
+            author="Neander de Souza"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+          />
+
+          <Post
+            author="John Doe"
+            content="Um novo post muito legal"
+          />
+        </main>
+      </div>      
     </div>
   )
 }
