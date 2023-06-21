4237830b7ad88a86175f1d2216234811141b45cc
Neander de Souza <neander.faria@hotmail.com>
2023-06-21 14:44:44 -0300

    Inserindo comentários (Programação declarativa)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react';
 import { format, formatDistanceToNow } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
 
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
@@ -11,7 +11,9 @@ export function Post({ author, publishedAt, content }) {
   const [comments, setComments] = useState([
     1,
     2,
+    'Post muito bacana, hein?!'
   ]);
+  const [newCommentText, setNewCommentText] = useState('');
 
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
@@ -25,7 +27,12 @@ export function Post({ author, publishedAt, content }) {
   function handleCrateNewComment() {
     event.preventDefault()
 
-    setComments([...comments, comments.length + 1]);
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value);
   }
   
   return (
@@ -60,7 +67,10 @@ export function Post({ author, publishedAt, content }) {
         <strong>Deixe seu feedback</strong>
 
         <textarea
+          name="comment"
           placeholder="Deixe um comentário"
+          value={newCommentText}
+          onChange={handleNewCommentChange}
         />
 
         <footer>
@@ -70,7 +80,7 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment />
+          return <Comment content={comment} />
         })}
       </div>
     </article>
