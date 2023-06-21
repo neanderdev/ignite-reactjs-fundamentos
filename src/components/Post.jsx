cf242060e7800a36defc019212239b4b799f8a1d
Neander de Souza <neander.faria@hotmail.com>
2023-06-21 14:47:17 -0300

    Comunicação entre componentes

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -34,6 +34,10 @@ export function Post({ author, publishedAt, content }) {
   function handleNewCommentChange() {
     setNewCommentText(event.target.value);
   }
+
+  function deleteComment(comment) {
+    console.log(`Deletar comentário ${comment}`)
+  }
   
   return (
     <article className={styles.post}>
@@ -80,7 +84,13 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment key={comment} content={comment} />
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          )
         })}
       </div>
     </article>
