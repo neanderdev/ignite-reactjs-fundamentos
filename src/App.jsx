45b401da52fea60effde074d45c383122a618aa7
Neander de Souza <neander.faria@hotmail.com>
2023-06-21 14:06:31 -0300

    Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { Post } from './Post'
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Neander de Souza"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+      />
+      
+      <Post
+        author="John Doe"
+        content="Um novo post muito legal"
+      />
     </div>
   )
 }
